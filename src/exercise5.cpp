cff1425659717e210a40b87358dc0cc092533206
Jemin Hwangbo <jhwangbo@kaist.ac.kr>
2023-05-26 15:36:13 +0900

    Update exercise5.cpp

--- src/exercise5.cpp
+++ src/exercise5.cpp
@@ -21,6 +21,7 @@ int main(int argc, char* argv[]) {
   Eigen::VectorXd gc(aliengo->getGeneralizedCoordinateDim()), gv(aliengo->getDOF()), gf(aliengo->getDOF());
   gc << 0, 0, 0.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8; /// Jemin: I'll randomize the gc, gv when grading
   gv << 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8;
+  gf << 0.15, 0.21, 0.36, 0.24, 0.35, 0.46, 0.57, 0.18, 0.29, 1.0, 1.1, 1.5, 1.1, 1.2, 1.3, 1.6, 1.7, 1.8;
   aliengo->setState(gc, gv);
   aliengo->setGeneralizedForce(gf);
 
